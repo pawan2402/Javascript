a883035da7b2b8bf3561ee525caccedd5dcac6d1
pawan2402 <pawanshekhawat2402@gmail.com>
2024-04-22 20:37:07 +0530

    Array

---
+++ array.js
@@ -0,0 +1,56 @@
+// <<<<<<<<<< Array Properties >>>>>>>>>>>>> //
+
+// Array length > To check the length of array
+
+// Array to String > Convert Array to String
+
+// Array at
+
+// Array Join
+
+// Array POP > To remove an element 
+
+// Array Push > To add an element
+
+// Array Shift > To add first element of array
+
+// Array Concatenate > Adding two arrays
+
+
+// taking array's data by innerhtml + checking length
+const lengthInnerHTML = ["mohit", "pawan", "sayar", "js", "html"];
+console.log(lengthInnerHTML.length);
+
+//array to string
+const arrayForString = ["mohit", "pawan", "sayar", "js", "html"];
+console.log(arrayForString.toString());
+
+// array at 
+const arrayForAt = ["mohit", "pawan", "sayar", "js", "html"];
+console.log(arrayForAt.at());
+
+// array joining 
+const arrayToJoin = ["mohit", "pawan", "sayar", "js", "html"];
+console.log(arrayForAt.join());
+
+// array pop 
+const arrayForPOP = ["mohit", "pawan", "sayar", "js", "html"];
+console.log(arrayForPOP.pop());
+
+// array push
+const arrayForPush = ["mohit", "pawan", "sayar", "js", "html"];
+console.log(arrayForPush.push('hello'));
+
+// array shift
+const arrayToShift = ["mohit", "pawan", "sayar", "js", "html"];
+console.log(arrayToShift.shift());
+
+// array unshift 
+const arrayToUnShift = ["mohit", "pawan", "sayar", "js", "html"];
+console.log(arrayToShift.unshift('mohit'));
+
+// array concinate
+const arrayToConcat1 = ["mohit", "pawan", "sayar", "js", "html"];
+const arrayToConcat2 = ["other", "members"];
+const concatenatedArray = arrayToConcat1.concat(arrayToConcat2);
+console.log(concatenatedArray);
